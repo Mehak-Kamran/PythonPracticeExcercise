f3e336cae9a1124a80df0fbae2a3ea3b7e99deb5
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-04-01 17:47:24 +0000

    ex7

---
+++ ex7.py
@@ -0,0 +1,6 @@
+'''Write a program to find how many times substring “Emma” appears 
+in the given string.'''
+
+stringx="Emma is a developer. Emma is a writer and emma likes to cook".lower()
+substring=stringx.count("emma")
+print(substring)
