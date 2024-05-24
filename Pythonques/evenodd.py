28d81ed372bf82463fcb936d3dc93bccf6771918
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-05-24 18:29:02 +0000

    ex1

---
+++ Pythonques/evenodd.py
@@ -0,0 +1,7 @@
+#Python Program to check if no is even or odd
+
+user_input=int(input("Enter a number: "))
+if user_input%2==0:
+    print(user_input,"is Even")
+else:
+    print(user_input,"is odd")
