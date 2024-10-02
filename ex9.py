d78e1a7d7ac1a0f3d76efd1658dc1bb57892111e
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-10-02 22:09:07 +0500

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -4,6 +4,6 @@ number=input("Enter a number: ")
 rev_num=number[::-1]
 print(rev_num)
 if number==rev_num:
-    print("No is Palindrome")
+    print("Number is Palindrome")
 else:
-    print("No is not a palindrome")
+    print("Number is not a palindrome")
